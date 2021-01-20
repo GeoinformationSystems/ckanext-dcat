ea50916b4e0848b0c485adb7a02c3581e048fff9
rue_a <arne.ruemmler@gmail.com>
2021-01-20 23:11:17 +0100

    temporal to dcat

--- ckanext/dcat/profiles.py
+++ ckanext/dcat/profiles.py
@@ -1417,9 +1417,9 @@ class GeoKurDCATAPProfile(EuropeanDCATAPProfile):
 
             g.add((temporal_extent, RDF.type, DCT.PeriodOfTime))
             if start:
-                self._add_date_triple(temporal_extent, SCHEMA.startDate, start)
+                self._add_date_triple(temporal_extent, DCAT.startDate, start)
             if end:
-                self._add_date_triple(temporal_extent, SCHEMA.endDate, end)
+                self._add_date_triple(temporal_extent, DCAT.endDate, end)
             g.add((dataset_ref, DCT.temporal, temporal_extent))
 
         # Spatial
