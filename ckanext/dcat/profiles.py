f8fec94c8bb7d50d49a4b3052ee783f41d3a92ef
rue_a <arne.ruemmler@gmail.com>
2021-03-08 17:07:11 +0100

    clu

--- ckanext/dcat/profiles.py
+++ ckanext/dcat/profiles.py
@@ -1823,8 +1823,7 @@ class GeoKurDCATAPProfile(EuropeanDCATAPProfile):
         if was_derived_from:
             activity_name = self._get_dataset_value(
                 dataset_dict, u'was_generated_by')
-            activity_ref = URIRef(eval(
-                'GKP.' + activity_name.replace(' ', '-')))
+            activity_ref = GKP.clu-mundo
             process_types = self._get_dataset_value(
                 dataset_dict, u'process_type')
             g.add((dataset_ref, RDF.type, PROV.Entity))
